f6f2e8932795c60f23d339517eecb0dfba384b77
Azri Afdhalluddin <azri.afdha@icloud.com>
2025-05-22 08:42:18 +0700

    Fix coo as manager signature error

--- app/Http/Controllers/Admin/PrintDemandController.php
+++ app/Http/Controllers/Admin/PrintDemandController.php
@@ -48,37 +48,50 @@ class PrintDemandController extends Controller
      */
     public function store(Request $request)
     {
-        // Pastikan ada checkbox yang dipilih
-        if (!$request->selected) {
-            return redirect()->back()->with('error', 'Silakan pilih setidaknya satu item untuk dicetak!');
+        // Jika Select All global aktif
+        if ($request->select_all == "1") {
+            // Ambil semua hasil query berdasarkan filter
+            $query = ItemDemand::where('status', 1);
+
+            if ($request->has('division_id') && $request->division_id) {
+                $query->whereHas('user', function ($q) use ($request) {
+                    $q->where('division_id', $request->division_id);
+                });
+            }
+
+            $approvedData = $query->with(['user.division', 'stationery'])->get();
+        } else {
+            // Jika hanya beberapa dipilih manual
+            if (!$request->selected) {
+                return redirect()->back()->with('error', 'Silakan pilih setidaknya satu item untuk dicetak!');
+            }
+
+            $selectedIds = explode(',', $request->selected);
+
+            $approvedData = ItemDemand::whereIn('id', $selectedIds)
+                ->where('status', 1)
+                ->get();
         }
 
-        $selectedIds = explode(',', $request->selected);
-
-        $approvedData = ItemDemand::whereIn('id', $selectedIds)
-            // ->where('coo_approval', 1)
-            ->where('status', 1)
-            ->get();
-
-        // Jika tidak ada data yang memenuhi syarat
         if ($approvedData->isEmpty()) {
             return redirect()->back()->with('error', 'Tidak ada data yang dapat dicetak!');
         }
 
-        $totalJumlah = $approvedData->sum('amount'); // Hitung total jumlah barang
-
+        $totalJumlah = $approvedData->sum('amount');
         $division = optional($approvedData->first())->user->division_id;
-        $manager = User::where('role', 'manager')->where('division_id', $division)->first();
+        // $manager = User::where('role', 'manager')->where('division_id', $division)->first();
+        $manager = User::where('role', 'manager')->where('division_id', $division)->first() ??
+            User::where('role', 'coo')->first();
         $coo = User::where('role', 'coo')->first();
         $admin = User::where('role', 'admin')->first();
 
-        // Gunakan Spatie untuk generate PDF
         $pdf = Pdf::view('pages.print.pengajuan_barang', compact('approvedData', 'manager', 'admin', 'coo', 'totalJumlah'))
             ->format('A4')
             ->name('pengajuan_barang.pdf');
 
-        return $pdf->inline(); // Tampilkan di browser
+        return $pdf->inline();
     }
+
 
 
 
